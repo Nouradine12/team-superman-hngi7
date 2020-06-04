25818b2562428f0b98d706751fa64a4ef086b8f7
Hachimi Dicko <hachimidicko1995@gmail.com>
2020-06-04 04:20:24 -0700

    Hachimi's Script

---
+++ scripts/hachimi.js
@@ -0,0 +1,8 @@
+const trainee = {
+    full_name:' Nouardine Hachimi DICKO',
+    id:'HNG-05104',
+    lang:'JavaScript',
+    email:'hachimidicko1995@gmail.com'
+  }
+  console.log('Hello world, this is ' +trainee.full_name+ ' with HNGi7 ID '+trainee.id +
+              ' using ' +trainee.lang+ 'for stage 2 task.' +trainee.email);
